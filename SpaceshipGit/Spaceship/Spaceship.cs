b3b073c85e54f7337c41602fefe92e0a15d6845e
SJHDijks <SJHDijks@users.noreply.github.com>
2019-09-18 23:25:07 +0200

    Spaceship.cs: Added datamembers. Added default constructor. Added custom constructor. Added empty Hit method. Added empty Move method.

--- SpaceshipGit/Spaceship/Spaceship.cs
+++ SpaceshipGit/Spaceship/Spaceship.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace Spaceship
 {
-    class Spaceship
+    abstract class Spaceship
     {
+        protected string name;
+        protected int health;
+        protected int maxHealth;
+        protected int fuel;
+        protected int maxFuel;
+        protected bool isAlive;
+
+        public Spaceship()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Spaceship(string name, int health, int fuel, bool isAlive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hit(int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

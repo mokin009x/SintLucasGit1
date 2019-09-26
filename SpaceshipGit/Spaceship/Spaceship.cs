c3eb3fe8dd5a3ef9cc971f616711e5556c77303a
Jaimy Magalhaes Coelho <35726400+mokin009x@users.noreply.github.com>
2019-09-26 15:32:48 +0200

    Spaceship: default constructor use case done. custom constructor use case done.

--- SpaceshipGit/Spaceship/Spaceship.cs
+++ SpaceshipGit/Spaceship/Spaceship.cs
@@ -15,16 +15,29 @@ namespace Spaceship
 
         public Spaceship()
         {
+            name = "SpaceShip";
+            health = 90;
+            maxHealth = health;
+            fuel = 100;
+            maxFuel = fuel;
+            isAlive = true;
             throw new NotImplementedException();
         }
 
-        public Spaceship(string name, int health, int fuel)
+        public Spaceship(string _name, int _health, int _fuel)
         {
+            name = _name;
+            health = _health;
+            maxHealth = health;
+            fuel = _fuel;
+            maxFuel = fuel;
+            isAlive = true;
             throw new NotImplementedException();
         }
 
         public void Hit(int damage)
         {
+            health = health - damage;
             throw new NotImplementedException();
         }
 
